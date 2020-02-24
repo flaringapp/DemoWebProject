281433d39d6db4bb2c5b5e2f471f81ec00924d34
Mallivance <ashpek01@gmail.com>
2020-02-24 15:58:47 +0200

    Add dynamic images add/remove

---
+++ js/images_controller.js
@@ -0,0 +1,39 @@
+const dynamicLogoLeft = document.getElementById("dynamic_logo_left");
+const dynamicLogoRight = document.getElementById("dynamic_logo_right");
+const dynamicLogos = [dynamicLogoLeft, dynamicLogoRight];
+
+const dynamicLogoImageId = "dynamic_logo_image"
+
+const imagePath = "images/fire.png";
+
+let isImageCreated = false;
+
+function toggleImages() {
+    if (isImageCreated) {
+        removeImages();
+    } else {
+        addImages();
+    }
+
+    isImageCreated = !isImageCreated;
+}
+
+function addImages() {
+    dynamicLogos.forEach(logo => logo.appendChild(createImage()));
+}
+
+function createImage() {
+    var image = document.createElement('img');
+    image.id = dynamicLogoImageId;
+    image.src = imagePath;
+    image.animation = 'initial'
+    return image;
+}
+
+function removeImages() {
+    dynamicLogos.forEach(logo => {
+        while (logo.firstChild) {
+            logo.removeChild(logo.firstChild);
+        }
+    });
+}
