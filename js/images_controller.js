0eee86b9b1d17473d6ebaa80de8dd101083b6260
Mallivance <ashpek01@gmail.com>
2020-02-25 13:41:15 +0200

    Add strange shit to manage images for Lab1

--- js/images_controller.js
+++ js/images_controller.js
@@ -1,3 +1,7 @@
+const staticLogo = document.getElementById("image_fire")
+const staticLogoIcon = document.getElementById("image_fire_icon")
+
+const staticLogoImage = document.getElementById("image_fire")
 const dynamicLogoLeft = document.getElementById("dynamic_logo_left");
 const dynamicLogoRight = document.getElementById("dynamic_logo_right");
 const dynamicLogos = [dynamicLogoLeft, dynamicLogoRight];
@@ -18,8 +22,23 @@ function toggleImages() {
     isImageCreated = !isImageCreated;
 }
 
+function onAddClick() {
+    addStaticImage();
+}
+
+function onRemoveClick() {
+    if (staticLogo.childElementCount > 1) {
+        staticLogo.removeChild(staticLogo.firstChild);
+    }
+}
+
 function addImages() {
     dynamicLogos.forEach(logo => logo.appendChild(createImage()));
+    addStaticImage();
+}
+
+function addStaticImage() {
+    staticLogo.insertBefore(createImage(), staticLogoIcon)
 }
 
 function createImage() {
@@ -37,4 +56,7 @@ function removeImages() {
             logo.removeChild(logo.firstChild);
         }
     });
+    while (staticLogo.childElementCount > 1) {
+        staticLogo.removeChild(staticLogo.firstChild);
+    }
 }
